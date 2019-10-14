a8182014757b7bebf0bb40060b64010435fa9efe
nita0018 <nita0018@stud.kea.dk>
2019-10-14 13:53:08 +0200

    Collapsibles

--- script.js
+++ script.js
@@ -115,3 +115,33 @@ function fastHeader() {
         header.classList.remove("sticky");
     }
 }
+
+//var coll = document.getElementsByClassName("collapsible");
+//var i;
+//
+//for (i = 0; i < coll.length; i++) {
+//    coll[i].addEventListener("click", function () {
+//        this.classList.toggle("active");
+//        var content = this.nextElementSibling;
+//        if (content.style.display === "block") {
+//            content.style.display = "none";
+//        } else {
+//            content.style.display = "block";
+//        }
+//    });
+//}
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
